b29bafc480c08f5587aa377c32b1b0eab873ad04
郝坤 <hk02335263@antgroup.com>
2025-09-22 13:22:56 +0800

    PullRequest: 455 modify torchair routing strategy
    Merge branch haokun/routing-modify-915 of git@code.alipay.com:Theta/vllm-ascend.git into dev-v0.10.2.915
    https://code.alipay.com/Theta/vllm-ascend/pull_requests/455
    
    Reviewed-by: 沧濯 <zhengshoujian.zsj@antgroup.com>
    
    
    * modify torchair routing strategy

--- vllm_ascend/torchair/quantization/torchair_w8a8_dynamic.py
+++ vllm_ascend/torchair/quantization/torchair_w8a8_dynamic.py
@@ -567,6 +567,9 @@ def torchair_fused_experts_with_allgather(hidden_states: torch.Tensor,
 
     hidden_states, pertoken_scale = torch_npu.npu_dynamic_quant(hidden_states)
 
+    group_list_type = 1
+    row_idx_type = 0
+
     hidden_states, expanded_x_idx, expert_tokens, pertoken_scale = torch_npu.npu_moe_init_routing_v2(
         hidden_states,
         topk_ids,
@@ -580,16 +583,7 @@ def torchair_fused_experts_with_allgather(hidden_states: torch.Tensor,
             ep_rank * local_num_experts, (ep_rank + 1) * local_num_experts
         ],
         quant_mode=-1,
-        row_idx_type=1)
-    group_list_type = 1
-
-    sorted_topk_weight = torch.index_select(topk_weights.view(-1), 0,
-                                            expanded_x_idx)
-    row_index = expanded_x_idx // topk_ids.shape[-1]
-    row_index = row_index.to(torch.int64)
-    share_input = torch.zeros((batch_size, hidden_size),
-                              dtype=torch.bfloat16,
-                              device="npu")
+        row_idx_type=row_idx_type)
 
     hidden_states = torch_npu.npu_grouped_matmul(
         x=[hidden_states],
@@ -613,17 +607,27 @@ def torchair_fused_experts_with_allgather(hidden_states: torch.Tensor,
         quant_mode=1,
     )
 
-    final_hidden_states = torch_npu.npu_grouped_matmul_finalize_routing(
-        hidden_states,
-        w2,
-        scale=w2_scale.to(torch.float32),
-        bias=None,
-        pertoken_scale=pertoken_scale.view(-1),
-        group_list=expert_tokens,
-        shared_input=share_input,
-        logit=sorted_topk_weight.to(torch.float32),
-        row_index=row_index,
-        output_bs=batch_size).to(torch.bfloat16)
+    hidden_states = torch_npu.npu_grouped_matmul(
+            [hidden_states], 
+            [w2], 
+            scale=[w2_scale], 
+            per_token_scale=[pertoken_scale], 
+            bias=None,
+            group_list=expert_tokens, 
+            split_item=3, 
+            output_dtype=torch.bfloat16,
+            group_type=0, 
+            group_list_type=group_list_type)[0]
+                
+    final_hidden_states = torch_npu.npu_moe_finalize_routing(
+        hidden_states.unsqueeze(1).to(torch.bfloat16),
+        None, 
+        None, 
+        None,
+        topk_weights.to(torch.bfloat16),
+        expanded_x_idx, 
+        topk_ids, 
+        drop_pad_mode=3)
 
     if len(original_shape) == 3:
         final_hidden_states = final_hidden_states.view(original_shape)
